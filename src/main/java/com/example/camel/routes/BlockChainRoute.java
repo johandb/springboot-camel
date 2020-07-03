982a6bcf84bd3957cc76cfb85b7d1e0d71192c5c
Johandenboer <johan.den.boer@ict.nl>
2020-07-03 15:23:50 +0200

    update the camel ride

--- src/main/java/com/example/camel/routes/BlockChainRoute.java
+++ src/main/java/com/example/camel/routes/BlockChainRoute.java
@@ -12,9 +12,7 @@ import org.springframework.stereotype.Component;
 import org.web3j.abi.EventEncoder;
 import org.web3j.abi.FunctionEncoder;
 import org.web3j.abi.TypeReference;
-import org.web3j.abi.datatypes.Function;
-import org.web3j.abi.datatypes.Type;
-import org.web3j.abi.datatypes.Uint;
+import org.web3j.abi.datatypes.*;
 import org.web3j.protocol.core.methods.response.EthBlock;
 
 import java.math.BigInteger;
@@ -89,6 +87,32 @@ public class BlockChainRoute extends RouteBuilder {
                 })
                 .end();
 
+        from("activemq:queue:consent")
+                .process(exchange -> {
+                    String body = exchange.getIn().getBody(String.class);
+                    log.info("BODY:{}", body);
+                    Operation operation = gson.fromJson(body, Operation.class);
+                    boolean consent = operation.getTxData().equalsIgnoreCase("true") ? true : false;
+                    log.info("value: {}", consent);
+                    Function function = new Function("setConsent",
+                            Arrays.<Type>asList(new Utf8String("1f4e22d9b6cc84622293adc79d3ba4e55721d99924616307ee4b59cb543162d9"), new Bool(consent)),
+                            Arrays.asList(new TypeReference<Bool>() {
+                            }));
+                    String encodedFunction = FunctionEncoder.encode(function);
+                    exchange.getIn().setHeader(OPERATION, ETH_SEND_TRANSACTION);
+                    exchange.getIn().setHeader(FROM_ADDRESS, "0x6e62f007992992DC7e0EA18208DCe4E273F8b898");
+                    exchange.getIn().setHeader(TO_ADDRESS, "0x4232Dd27e975DA918363EbA39caC883f5312513b");
+                    exchange.getIn().setHeader(AT_BLOCK, "latest");
+                    exchange.getIn().setHeader(GAS_LIMIT, "6721975");
+                    exchange.getIn().setHeader(GAS_PRICE, "21000");
+                    exchange.getIn().setHeader(DATA, encodedFunction);
+                })
+                .to("web3j://http://127.0.0.1:7545")
+                .process(exchange -> {
+                    log.info("TX:{}", exchange.getIn().getBody());
+                })
+                .end();
+
         from("web3j://http://127.0.0.1:7545?operation=ETH_LOG_OBSERVABLE&topics=" + topics)
                 .setHeader(OPERATION, constant(ETH_SEND_TRANSACTION))
                 .setHeader(FROM_ADDRESS, constant("0x6e62f007992992DC7e0EA18208DCe4E273F8b898"))
@@ -110,4 +134,5 @@ public class BlockChainRoute extends RouteBuilder {
                 })
                 .end();
     }
+
 }
